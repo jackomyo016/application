26ab9bda9e693e9f5e6c5afe53ae05ee8b948043
jackomyo016 <jacky-vivien.mpoy@polytechnique.edu>
2025-09-03 01:58:08 +0000

    secrets correctionbis_final

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ data_test_path = os.environ.get("test_path", "data/derived/test.parquet")
 MAX_DEPTH = None
 MAX_FEATURES = "sqrt"
 
-if jeton_api.startswith("$"):
+if jeton_api.startswith(""):
     logger.info("API token has been configured properly")
 else:
     logger.warning("API token has not been configured")
