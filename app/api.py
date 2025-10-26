78e9b46522def8152b5912324d2f6aaa2d912d54
jackomyo016 <jacky-vivien.mpoy@polytechnique.edu>
2025-10-26 04:19:59 +0000

    Tags 4

--- app/api.py
+++ app/api.py
@@ -6,7 +6,12 @@ import joblib
 from typing import Optional
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Démonstration du modèle de prédiction de survie sur le Titanic",
+    description=
+    "<b>Application de prédiction de survie sur le Titanic</b> 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
+        "<br><br><img src=\"https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg\" width=\"200\">"
+    )
 model = joblib.load("model.joblib")
 
 @app.get("/")
