ac2b578a9551dc069d3efca8c4838b1d4fe9a145
jackomyo016 <jacky-vivien.mpoy@polytechnique.edu>
2025-11-01 18:24:35 +0000

    version 5

--- train.py
+++ train.py
@@ -27,6 +27,13 @@ parser.add_argument(
 )
 
 parser.add_argument(
+    "--max_features",
+    type=str, default="sqrt",
+    choices=['sqrt', 'log2'],
+    help="Number of features to consider when looking for the best split"
+)
+
+parser.add_argument(
     "--experiment_name", type=str, default="titanicML", help="MLFlow experiment name"
 )
 args = parser.parse_args()
@@ -39,7 +46,8 @@ data_path = os.environ.get("data_path", URL_RAW)
 data_train_path = os.environ.get("train_path", "data/derived/train.parquet")
 data_test_path = os.environ.get("test_path", "data/derived/test.parquet")
 MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
+MAX_FEATURES = args.max_features
+
 
 if jeton_api.startswith("$"):
     logger.info("API token has been configured properly")
